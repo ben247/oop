7986b4e0ff63b3e0f392f84d2bdc427f999a5e1b
Ben Whalley <ben247@gmail.com>
2018-09-29 16:12:15 +0100

    oop classes, methods and properties

---
+++ challenge_01.php
@@ -0,0 +1,15 @@
+<?php
+
+class Bycicle {
+    var $brand;
+    var $model;
+    var $year;
+    var $description;
+    var $weight_kg;
+
+    function name() {
+        return 'Hello World!';
+    }
+}
+
+?>
