bcecb178f00b505f3438254b1fd2b967edd3d4b4
ben whalley <ben247@gmail.com>
2018-09-30 15:49:34 +0100

    Subclasses

--- challenge_01.php
+++ challenge_01.php
@@ -1,6 +1,6 @@
 <?php
 
-class Bycicle {
+class Bicycle {
     var $brand;
     var $model;
     var $year;
@@ -12,8 +12,34 @@ class Bycicle {
     }
 
     function weight_lbs() {
-        retun floatval($this->weight_kg) * 2.2046226218;
+        return floatval($this->weight_kg) * 2.2046226218;
+    }
+
+    function set_weight_lbs($value) {
+        $this->weight_kg = floatval($value) / 2.2046226218;
     }
 }
+
+$bicycle1 = new Bicycle;
+$bicycle1->brand = 'Nukeproof';
+$bicycle1->model = 'Mega';
+$bicycle1->year = '2018';
+$bicycle1->weight_kg = 1.0;
+
+$bicycle2 = new Bicycle;
+$bicycle2->brand = 'Cube';
+$bicycle2->model = 'Gyro';
+$bicycle2->weight_kg = 2.0;
+$bicycle2->year = '2017';
+
+echo $bicycle1->name() . "<br />";
+echo $bicycle2->name() . "<br />";
+
+echo $bicycle2->weight_kg . "<br />";
+echo $bicycle2->weight_lbs() . "<br />";
+
+$bicycle1->weight_lbs(2);
+echo $bicycle2->weight_kg . "<br />";
+echo $bicycle2->weight_lbs() . "<br />";
 
 ?>
