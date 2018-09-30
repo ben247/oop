1fd30ffc5870d583df4472d6fc9312ec8c31c442
ben whalley <ben247@gmail.com>
2018-09-30 14:39:43 +0100

    update

--- challenge_01.php
+++ challenge_01.php
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-class Bicycle {
-    var $brand;
-    var $model;
-    var $year;
-    var $description = 'Used bicycle';
-    var $weight_kg = 0.0;
-
-    function name() {
-        return $this->brand . " " . $this->model . " (" . $this->year . ")";
-    }
-
-    function weight_lbs(){
-        return floatval($this->weight_kg) * 2.2046226218;
-    }
-}
-
-$bicycle1 = new Bicycle;
-$bicycle1->brand = 'Nukeproof';
-$bicycle1->model = 'Mega';
-$bicycle1->year = '2018';
-$bicycle1->weight_kg = 140;
-
-echo $bicycle1->name() . "<br />";
-echo $bicycle1->weight_kg . "<br />";
-echo $bicycle1->weight_lbs() . "<br />";
-
-=======
 <?php
 
 class Bycicle {
@@ -38,9 +8,12 @@ class Bycicle {
     var $weight_kg;
 
     function name() {
-        return 'Hello World!';
+        return $this->brand . " " . $this->model . " (" . $this->year . ")";
+    }
+
+    function weight_lbs() {
+        retun floatval($this->weight_kg) * 2.2046226218;
     }
 }
 
->>>>>>> master
 ?>
